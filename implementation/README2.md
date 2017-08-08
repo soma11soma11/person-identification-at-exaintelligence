b625ab2461609a3b542c718fc4eff4a8f5f7fc3a
soma11soma11 <h2so4cos0@gmail.com>
2017-08-08 15:07:36 +0900

    tidy up personal identification

---
+++ implementation/README2.md
@@ -0,0 +1,17 @@
+# Implementaion-1
+
+Implementation for CVPR 2015 Paper: "An Improved Deep Learning Architecture for Person Re-Identification".
+
+[`Paper link`](http://www.cv-foundation.org/openaccess/content_cvpr_2015/papers/Ahmed_An_Improved_Deep_2015_CVPR_paper.pdf)
+
+This architechture is implemented based on `Keras` with `Tensorflow` backen using `Python` Programming Language.
+
+## Train the model on which dataset?
+
+1. [`Market-1501`](https://github.com/Deep-Learning-Person-Re-Identification/Implementaion-1/tree/master/market1501)
+
+2. [`CUHK-03`](https://github.com/Deep-Learning-Person-Re-Identification/Implementaion-1/tree/master/CUHK03)
+
+## The model structure
+
+![](https://github.com/Deep-Learning-Person-Re-Identification/Implementaion-1/blob/master/model.png)
---
+++ implementation/code/CUHK03/make_hdf5_for_cuhk03.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+import h5py
+import numpy as np
+from PIL import Image
+
+def make_hdf5_for_cuhk03(file_path = '/home/lpc/dataset/cuhk-03.mat'):
+    
+    with h5py.File(file_path,'r') as f, h5py.File('cuhk-03.h5') as fw:
+        val_index = (f[f['testsets'][0][0]][:].T - 1).tolist()
+        tes_index = (f[f['testsets'][0][1]][:].T - 1).tolist()
+        
+        fwa = fw.create_group('a')
+        fwb = fw.create_group('b')
+        fwat = fwa.create_group('train')
+        fwav = fwa.create_group('validation')
+        fwae = fwa.create_group('test')
+        fwbt = fwb.create_group('train')
+        fwbv = fwb.create_group('validation')
+        fwbe = fwb.create_group('test')
+        
+        temp = []
+        count_t = 0
+        count_v = 0
+        count_e = 0
+        for i in range(3):
+            for k in range(f[f['labeled'][0][i]][0].size):
+                print (i,k)
+                if [i,k] in val_index:
+                    for j in range(5):
+                        if len(f[f[f['labeled'][0][i]][j][k]].shape) == 3:
+                            temp.append(np.array((Image.fromarray(f[f[f['labeled'][0][i]][j][k]][:].transpose(2,1,0))).resize((64,128))) / 255.)
+                    fwav.create_dataset(str(count_v),data = np.array(temp))
+                    temp = []
+                    for j in range(5,10):
+                        if len(f[f[f['labeled'][0][i]][j][k]].shape) == 3:
+                            temp.append(np.array((Image.fromarray(f[f[f['labeled'][0][i]][j][k]][:].transpose(2,1,0))).resize((64,128))) / 255.)
+                    fwbv.create_dataset(str(count_v),data = np.array(temp))
+                    temp = []
+                    count_v += 1
+                if [i,k] in tes_index:
+                    for j in range(5):
+                        if len(f[f[f['labeled'][0][i]][j][k]].shape) == 3:
+                            temp.append(np.array((Image.fromarray(f[f[f['labeled'][0][i]][j][k]][:].transpose(2,1,0))).resize((64,128))) / 255.)
+                    fwae.create_dataset(str(count_e),data = np.array(temp))
+                    temp = []
+                    for j in range(5,10):
+                        if len(f[f[f['labeled'][0][i]][j][k]].shape) == 3:
+                            temp.append(np.array((Image.fromarray(f[f[f['labeled'][0][i]][j][k]][:].transpose(2,1,0))).resize((64,128))) / 255.)
+                    fwbe.create_dataset(str(count_e),data = np.array(temp))
+                    temp = []
+                    count_e += 1
+                if [i,k] not in val_index and [i,k] not in tes_index:
+                    for j in range(5):
+                        if len(f[f[f['labeled'][0][i]][j][k]].shape) == 3:
+                            temp.append(np.array((Image.fromarray(f[f[f['labeled'][0][i]][j][k]][:].transpose(2,1,0))).resize((64,128))) / 255.)
+                    fwat.create_dataset(str(count_t),data = np.array(temp))
+                    temp = []
+                    for j in range(5,10):
+                        if len(f[f[f['labeled'][0][i]][j][k]].shape) == 3:
+                            temp.append(np.array((Image.fromarray(f[f[f['labeled'][0][i]][j][k]][:].transpose(2,1,0))).resize((64,128))) / 255.)
+                    fwbt.create_dataset(str(count_t),data = np.array(temp))
+                    temp = []
+                    count_t += 1
+                    
+if __name__ == '__main__':
+    user_name = input('please input your system user name:')
+    if user_name == 'lpc':
+        make_hdf5_for_cuhk03()
+    else:
+        make_hdf5_for_cuhk03('../../'+user_name+'/cuhk-03.mat')
